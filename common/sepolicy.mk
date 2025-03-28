3bae4a02687fbc6212b0b075ad1946e744ffa7d8
wulan17 <wulan17@nusantararom.org>
2025-03-28 19:22:40 +0700

    sepolicy: Initial KomodoOS
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,22 +10,22 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/komodo/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/komodo/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/komodo/sepolicy/common/dynamic \
+    device/komodo/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/komodo/sepolicy/common/dynamic \
+    device/komodo/sepolicy/common/vendor
 endif
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/komodo/sepolicy/atv/sepolicy.mk
 endif
